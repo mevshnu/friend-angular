60e09cd40a75dc2b12bf63271fcc716cc92aa24c
mevshnu <vishnusivan1000@gmail.com>
2022-11-28 12:08:39 +0530

    opo

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -10,4 +10,10 @@ export class AddComponent {
   friendName=""
   friendNickName=""
   DescribeYourFriend=""
+
+  readValue =()=>
+  {
+    let data:any= {"name":this.name,"friendName":this.friendName,"friendNickName":this.friendNickName,"DescribeYourFriend":this.DescribeYourFriend}
+  console.log(data)
+  }
 }
